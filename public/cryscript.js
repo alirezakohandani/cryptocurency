87c16ada0ef8e6f4ca2d6d4f5d4aa2cde6460db0
alirezakohandani <alireza.ko7@gmail.com>
2018-10-19 09:13:12 +0330

    third commit commit (add angular)

--- public/cryscript.js
+++ public/cryscript.js
@@ -1,6 +1,13 @@
 function username1() {
-var x = document.getElementById("username1");
+    var x = document.getElementById("#username1");
 if (x == "") {
     alert('kouni');
 }
+}
+
+function change_coin() {
+    var x = input.change.value;
+    var y = x * 10;
+
+
 }
